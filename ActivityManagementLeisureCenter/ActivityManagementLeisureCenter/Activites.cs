442bc75c445f6b3148289518b09441794e71543e
Jason Gauthier <jason.gauthier.03@edu.cegeptr.qc.ca>
2024-11-29 14:49:19 -0500

    Class Activite & SingletonBD

---
+++ ActivityManagementLeisureCenter/ActivityManagementLeisureCenter/Activites.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityManagementLeisureCenter
+{
+    class Activites
+    {
+        string nom, type;
+        decimal cout_organisation, prix_vente;
+
+        public Activites()
+        {
+
+        }
+
+        public Activites(string nom, string type, decimal cout_organisation, decimal prix_vente)
+        {
+            Nom = nom;
+            Type = type;
+            Cout_organisation = cout_organisation;
+            Prix_vente = prix_vente;
+        }
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Type { get => type; set => type = value; }
+        public decimal Cout_organisation { get => cout_organisation; set => cout_organisation = value; }
+        public decimal Prix_vente { get => prix_vente; set => prix_vente = value; }
+    }
+}
---
+++ ActivityManagementLeisureCenter/ActivityManagementLeisureCenter/SingletonBD.cs
@@ -0,0 +1,62 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityManagementLeisureCenter
+{
+    class SingletonBD
+    {
+        private MySqlConnection con;
+        private ObservableCollection<Activites> listeActivites;
+        private static SingletonBD instance = null;
+
+        public SingletonBD()
+        {
+            con = new MySqlConnection("Server=cours.cegep3r.info;Database=a2024_420335ri_gr1_2361208-jason-gauthier;Uid=2361208;Pwd=2361208;");
+            listeActivites = new ObservableCollection<Activites>();
+        }
+
+        public static SingletonBD getInstance()
+        {
+            if (instance == null)
+                instance = new SingletonBD();
+
+            return instance;
+        }
+
+        public ObservableCollection<Activites> getListeActivites()
+        {
+            return listeActivites;
+        }
+
+        // Méthode pour aller chercher toutes les activitées
+        public void chargerActivites()
+        {
+            listeActivites.Clear();
+            MySqlCommand commande = new MySqlCommand("SELECT * FROM activite", con);
+
+            con.Open();
+            MySqlDataReader reader = commande.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string nom = reader.GetString("nom");
+                string type = reader.GetString("type");
+                decimal cout_organisation = reader.GetDecimal("cout_organisation");
+                decimal prix_vente = reader.GetDecimal("prix_vente");
+
+                Activites activite = new Activites(nom, type, cout_organisation, prix_vente);
+                listeActivites.Add(activite);
+            }
+            reader.Close();
+            con.Close();
+        }
+
+
+
+    }
+}
