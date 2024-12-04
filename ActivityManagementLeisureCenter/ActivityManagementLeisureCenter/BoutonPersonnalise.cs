de3e68e3dac54f923a8fce823913c16eae9216bf
jayfire1 <jason.gauthier.03@edu.cegeptr.qc.ca>
2024-12-03 23:00:07 -0500

    Création de la classe BoutonPersonnalise et modifier le bouton d'inscription

---
+++ ActivityManagementLeisureCenter/ActivityManagementLeisureCenter/BoutonPersonnalise.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.UI.Input;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityManagementLeisureCenter
+{
+    public class BoutonPersonnalise : Button
+    {
+        public BoutonPersonnalise()
+        {
+            this.PointerEntered += BoutonPersonnalise_PointerEntered;
+            this.PointerExited += BoutonPersonnalise_PointerExited;
+        }
+
+        private void BoutonPersonnalise_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            this.ProtectedCursor = InputSystemCursor.Create(InputSystemCursorShape.Hand);
+        }
+
+        private void BoutonPersonnalise_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            this.ProtectedCursor = InputSystemCursor.Create(InputSystemCursorShape.Arrow);
+        }
+    }
+}
