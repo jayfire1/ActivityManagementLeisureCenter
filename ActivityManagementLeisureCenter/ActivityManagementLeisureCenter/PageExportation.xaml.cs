1da4be02fc29f0b4ee5a3cdb00dc23c2758a3220
jayfire1 <jason.gauthier.03@edu.cegeptr.qc.ca>
2024-12-07 19:15:08 -0500

    Page Exportation Terminer

--- ActivityManagementLeisureCenter/ActivityManagementLeisureCenter/PageExportation.xaml.cs
+++ ActivityManagementLeisureCenter/ActivityManagementLeisureCenter/PageExportation.xaml.cs
@@ -86,9 +86,58 @@ namespace ActivityManagementLeisureCenter
         }
 
 
-        private void ExporterActivitesClick(object sender, RoutedEventArgs e)
+        private async void ExporterActivitesClick(object sender, RoutedEventArgs e)
         {
+            var picker = new Windows.Storage.Pickers.FileSavePicker();
 
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(HelperClass.mainWindow);
+            WinRT.Interop.InitializeWithWindow.Initialize(picker, hWnd);
+
+            picker.SuggestedFileName = "Liste_Activites";
+            picker.FileTypeChoices.Add("Fichier texte", new List<string>() { ".csv" });
+
+            // Crée le fichier
+            Windows.Storage.StorageFile monFichier = await picker.PickSaveFileAsync();
+
+            // Réinitialise les messages de succès et d'erreur avant de commencer
+            SuccessMessage.Text = "";
+            ErrorMessage.Text = "";
+
+            if (monFichier != null)
+            {
+                ObservableCollection<Activites> activitesList = SingletonBD.getInstance().getListeActivites();
+
+                // Vérifiez si la liste est vide ou nulle
+                if (activitesList == null || activitesList.Count == 0)
+                {
+                    ErrorMessage.Text = "Aucune activité trouvé.";
+                    return;
+                }
+
+                // Préparer les lignes à écrire dans le fichier CSV
+                List<string> lignes = new List<string> { "IdActivite;Nom;Type;Cout_organisation;PrixVenteFormatte;Image" };
+
+                // Boucle pour ajouter les données des adhérents
+                foreach (var activite in activitesList)
+                {
+                    lignes.Add($"{activite.IdActivite};{activite.Nom};{activite.Type};{activite.Cout_organisation};{activite.PrixVenteFormatte};{activite.Image}");
+                }
+
+                // Écriture des lignes dans le fichier CSV
+                try
+                {
+                    await Windows.Storage.FileIO.WriteLinesAsync(monFichier, lignes, Windows.Storage.Streams.UnicodeEncoding.Utf8);
+                    SuccessMessage.Text = "Exportation réussie !";
+                }
+                catch (Exception ex)
+                {
+                    ErrorMessage.Text = $"Erreur lors de l'exportation : {ex.Message}";
+                }
+            }
+            else
+            {
+                ErrorMessage.Text = "Aucun fichier sélectionné.";
+            }
         }
 
     }
